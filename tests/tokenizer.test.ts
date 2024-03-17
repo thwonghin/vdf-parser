5de0543fe78243d8c5b4a2f9d1be4c94a9d3f557
Hin Wong <13003836+thwonghin@users.noreply.github.com>
2024-03-17 08:28:04 -0400

    test: fix import

--- tests/tokenizer.test.ts
+++ tests/tokenizer.test.ts
@@ -6,7 +6,6 @@ import {
     Tokenizer,
     TokenizerCloseBracketAfterKeyError,
     TokenizerEscapeOutsideQuote,
-    TokenizerNoCharacterToEscapeError,
     TokenizerOpenBracketAfterValueError,
     TokenizerTooManyBracketsError,
     TokenizerUnsupportedEscapeSequenceError,
